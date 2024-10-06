eacc0487bb18ff008aeb53216d428cf3a8f58b4d
DxTea <dexter_010@mail.ru>
2024-10-06 21:31:49 +0500

    Dockerfile

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -127,3 +127,9 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     )
 }
+
+from datetime import timedelta
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=365 * 100),  # 100 years
+}
