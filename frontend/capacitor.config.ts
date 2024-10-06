a5e1727807d6f37cca8fbfa30b2269043527d757
vvnikita74 <124995385+vvnikita74@users.noreply.github.com>
2024-10-06 19:23:53 +0300

    front initial

---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,24 @@
+import type { CapacitorConfig } from '@capacitor/cli'
+
+const config: CapacitorConfig = {
+	appId: 'com.spoural.excelapp',
+	appName: 'ExcelApp',
+	webDir: 'build',
+	// server: {
+	// 	url: 'http://192.168.0.176:3000',
+	// 	cleartext: true,
+	// },
+	plugins: {
+		SafeArea: {
+			enabled: true,
+			customColorsForSystemBars: true,
+			statusBarColor: '#000000',
+			statusBarContent: 'light',
+			navigationBarColor: '#000000',
+			navigationBarContent: 'light',
+			offset: 0,
+		},
+	},
+}
+
+export default config
