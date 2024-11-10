088cc385c5f77c422f4fb8e39751c9d72d3c1860
DxTea <dexter_010@mail.ru>
2024-11-11 02:35:23 +0500

    upd

--- backend/apps/excel_app/utils/report_processing.py
+++ backend/apps/excel_app/utils/report_processing.py
@@ -1,8 +1,9 @@
 import os
 import inspect
+import re
 import pymorphy2
 import openpyxl
-
+from django.conf import settings
 from datetime import datetime
 from apps.excel_app.models import Sheet
 
@@ -47,21 +48,7 @@ def generate_report(data, username):
         for cell_data in sheet.get_data():
             cell = ws[cell_data.index]
             template = cell_data.template
-            for key, value in data.items():
-                placeholder = f'${key}$'
-                if placeholder in template:
-                    template = template.replace(placeholder, str(value))
-            for part in template.split('$'):
-                if '.' in part:
-                    word, key = part.split('.')
-                    if key in data:
-                        initial = data[key]
-                        gender = get_gender(initial.split(' ')[-1])
-                        if gender:
-                            inflected_word = change_gender(word, gender)
-                            if inflected_word:
-                                template = template.replace(f'${word}.{key}$',
-                                                            inflected_word)
+            template = substitute_placeholders(template, data)
             cell.value = template
 
         if sheet.countCell:
@@ -72,15 +59,55 @@ def generate_report(data, username):
     if wb.worksheets:
         wb.remove(wb.worksheets[-1])
 
-    report_dir = os.path.join(os.path.dirname(__file__), '..', 'media',
-                              'reports')
+    report_dir = os.path.join(settings.MEDIA_ROOT, 'reports')
     os.makedirs(report_dir, exist_ok=True)
     report_full_filename = os.path.join(report_dir,
                                         f'{datetime.now().strftime("%H-%M_%d.%m.%Y")}-{username}.xlsx')
     wb.save(report_full_filename)
-
-    os.system(
-        f'libreoffice --headless --convert-to pdf {report_full_filename}')
-
     report_filename = os.path.basename(report_full_filename)
+    os.system(f'libreoffice --headless --convert-to pdf --outdir'
+              f' {report_dir} {report_filename}')
     return report_filename
+
+
+def substitute_placeholders(template, data):
+    gentArr = {
+        "Родительный": "gent",
+        "Именительный": "nomn",
+        'Дательный': 'datv',
+        'Винительный': 'accs',
+        'Творительный': 'ablt',
+        'Предложный': 'loct',
+        'Звательный': 'voct',
+    }
+
+    def replace_match(match):
+        key = match.group(1)
+        keyArr = key.split('.')
+        length = len(keyArr)
+        match length:
+            case 1:
+                return data.get(key, f'${key}$')
+            case 2:
+                word, key = keyArr
+                if key in gentArr:
+                    initial = data.get(word, '')
+                    gender = get_gender(initial.split(' ')[-1])
+                    if gender:
+                        inflected_word = change_gender(word, gender)
+                        if inflected_word:
+                            return inflected_word
+                elif key in data:
+                    initial = data[key]
+                    gender = get_gender(initial.split(' ')[-1])
+                    if gender:
+                        inflected_word = change_gender(word, gender)
+                        if inflected_word:
+                            return inflected_word
+            case 3:
+                # Обработка случая с тремя частями в ключе
+                pass
+
+        return data.get(key, f'${key}$')
+
+    return re.sub(r'\$(\w+(\.\w+)*)\$', replace_match, template)
