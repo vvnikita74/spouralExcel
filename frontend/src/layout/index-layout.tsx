e5c600dbe0ff5715fcb0da537d3c4953fc0b5a07
vvnikita74 <124995385+vvnikita74@users.noreply.github.com>
2024-12-05 23:17:24 +0300

    Update index-layout.tsx

--- frontend/src/layout/index-layout.tsx
+++ frontend/src/layout/index-layout.tsx
@@ -9,8 +9,8 @@ export default function IndexLayout() {
 		<GlobalContext>
 			<main className='relative size-full overflow-hidden p-4'>
 				<div
-					className='top-[calc(var(--safe-area-inset-top, 0px)+1rem)] sticky flex
-						w-full flex-row justify-between'>
+					className='sticky top-[calc(var(--safe-area-inset-top,0px))] flex w-full
+						flex-row justify-between'>
 					<Link to='/'>
 						<img
 							src={logoIcon}
@@ -21,7 +21,7 @@ export default function IndexLayout() {
 					</Link>
 					<ProfilePanel />
 				</div>
-				<div className='scrollbar-hide mt-4 h-[calc(100dvh-4.5rem)] overflow-scroll'>
+				<div className='scrollbar-hide mt-4 h-[calc(100dvh-4.5rem)] overflow-scroll pb-4'>
 					<Outlet />
 				</div>
 			</main>
