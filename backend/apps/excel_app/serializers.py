05ae5ed5ed74daf3c407ac67a14f19f0f103f6e5
DxTea <dexter_010@mail.ru>
2024-12-23 21:34:59 +0500

    правки админки, вроде робит

--- backend/apps/excel_app/serializers.py
+++ backend/apps/excel_app/serializers.py
@@ -1,16 +1,9 @@
 from rest_framework import serializers
 from .models import Fields
 
-
 class FieldsSerializer(serializers.ModelSerializer):
-    # related_objects = RelatedModelSerializer(many=True, read_only=True)
+    settings = serializers.JSONField()
 
     class Meta:
         model = Fields
         fields = '__all__'
-
-# For relations model
-# class RelatedModelSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = RelatedModel
-#         fields = '__all__'
