b70823d62a3d96719a539e43b155cc55ab04a357
vvnikita74 <124995385+vvnikita74@users.noreply.github.com>
2025-01-06 14:25:45 +0300

    Update serializers.py

--- backend/apps/excel_app/serializers.py
+++ backend/apps/excel_app/serializers.py
@@ -3,9 +3,6 @@ from .models import Fields
 
 class FieldsSerializer(serializers.ModelSerializer):
     settings = serializers.JSONField()
-
-class FieldsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Fields
         fields = '__all__'
-        list_serializer_class = FieldsSerializer
