3baf81b98c1bdcc29af442e358cf89d723fd1fa1
DxTea <dexter_010@mail.ru>
2025-01-21 21:57:03 +0500

    upd

--- backend/apps/user/admin.py
+++ backend/apps/user/admin.py
@@ -3,19 +3,27 @@ from .models import UserData, UserDataFile
 from .forms import UserDataAdminForm
 
 
+class UserDataFileInline(admin.TabularInline):
+    model = UserDataFile
+    extra = 0
+
+
 class UserDataAdmin(admin.ModelAdmin):
     form = UserDataAdminForm
     list_display = ('get_username', 'dateCreated', 'isReady')
     ordering = ('-dateCreated',)
+    inlines = [UserDataFileInline]
 
     def get_username(self, obj):
         return obj.user.username
 
     get_username.short_description = 'Username'
 
+
 class UserFilesAdmin(admin.ModelAdmin):
     list_display = ('user_data_id', 'file', 'key')
     ordering = ('user_data',)
+
 
 admin.site.register(UserData, UserDataAdmin)
 admin.site.register(UserDataFile, UserFilesAdmin)
