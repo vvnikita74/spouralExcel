0038dd31c49526595e51246b7a61ef1fb2ee7020
DxTea <dexter_010@mail.ru>
2025-01-30 23:59:24 +0500

    defects & recs to /data

--- backend/apps/excel_app/serializers.py
+++ backend/apps/excel_app/serializers.py
@@ -1,9 +1,28 @@
 from rest_framework import serializers
-from .models import Fields
+from .models import Fields, Recommendations
+from .models import ConstructionTypes, Materials, Defects
+
+
+class MaterialSerializer(serializers.ModelSerializer):
+    defects = serializers.StringRelatedField(many=True)
+    recs = serializers.StringRelatedField(many=True, source='recommendations')
+
+    class Meta:
+        model = Materials
+        fields = ['name', 'defects', 'recs']
+
+
+class ConstructionTypeSerializer(serializers.ModelSerializer):
+    materials = MaterialSerializer(many=True, source='material')
+
+    class Meta:
+        model = ConstructionTypes
+        fields = ['name', 'materials']
 
 
 class FieldsSerializer(serializers.ModelSerializer):
     settings = serializers.JSONField()
+    construction_type = ConstructionTypeSerializer()
 
     class Meta:
         model = Fields
